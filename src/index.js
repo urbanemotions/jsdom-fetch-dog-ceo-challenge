d082d115f3f1eb517a849bbdb690d6452c0ad228
sin wan wildy <sw.wildy@gmail.com>
2021-03-01 09:52:53 -0800

    commit tree b8a83a9ee2e1534db00c1fdb5965baf527355bdb

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function()
+{
+    loadImages();
+    loadBreedOptions();
+});
+
+function loadImages()
+{
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(function(res)
+    {
+        res.json()
+    })
+    .then(function(results) 
+    {
+        results.message.forEach(function(image) {addImage(image)} )
+    });
+}
+
+function loadBreedOptions()
+{
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+}
+
